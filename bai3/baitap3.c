2514ab523d1db10c6415b3b52e39e70f2b75a1f2
Shinosuke <hoangbach11299@gmail.com>
2023-09-08 20:54:28 +0700

    bai tap

---
+++ bai3/baitap3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a,b,c,d;
+	a=8;
+	b=2;
+	c=10;
+	d = 10 * a * a + 5 * b * c + c * c;
+	printf("gia tri d =  %d",d);
+	
+}
